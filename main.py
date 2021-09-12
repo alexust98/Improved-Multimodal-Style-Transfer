dd44f7dd4d28b3c2c1ad5fa691a1c69562778d36
alexust98 <alex.ustyuzhanin98@yandex.ru>
2021-09-12 18:37:46 +0300

    Initial commit with preliminary code architecture

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+import argparse
+from PIL import Image
+import numpy as np
+import torch
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from skimage.io import imsave
+from time import time
+
+from src.model import IMST
+from src.utils import normalize_mask, tensor_to_array
+
+def main():
+	parser = argparse.ArgumentParser(description='Structure-emphasized Multimodal Style Transfer by CHEN CHEN')
+	parser.add_argument('--content', '-c', type=str, default=None,
+						help='Content image path e.g. content.jpg')
+	parser.add_argument('--style', '-s', type=str, default=None,
+						help='Style image path e.g. image.jpg')
+	parser.add_argument('--output_name', '-o', type=str, default="results/result.jpg",
+						help='Output path for generated image, no need to add ext, e.g. out')
+	parser.add_argument('--imsize', type=int, default=512,
+						help='Size of content and style images to be scaled to. E.g. 512x512. Non-square shapes are not supported yet.')
+	parser.add_argument('--WCT_alpha', '-alpha', default=1.0,
+						help='WCT procedure content/style fusion proportion, e.g. 1.0 means full stylization, 0.0 would return content image')
+	parser.add_argument('--gpu', type=int, default=0,
+						help='GPU device id')
+	parser.add_argument('--model_path', type=str, default='model/model_state.pth',
+						help='pretrained model path')
+
+	args = parser.parse_args()
+
+	# set device on GPU if available, else CPU
+	if (torch.cuda.is_available()) and (args.gpu >= 0):
+		device = torch.device(f'cuda:{args.gpu}')
+		print(f'Working on CUDA: {torch.cuda.get_device_name(0)}\n')
+	else:
+		device = 'cpu'
+		print("Working on CPU\n")
+
+
+	model = IMST(alpha=args.WCT_alpha, device=device)
+	model.load_state_dict(torch.load(args.model_path, map_location=lambda storage, loc: storage))
+	model = model.to(device)
+	
+	c = Image.open(args.content).convert('RGB')
+	s = Image.open(args.style).convert('RGB')
+	c = np.array(transforms.Resize((args.imsize, args.imsize))(c))
+	s = np.array(transforms.Resize((args.imsize, args.imsize))(s))
+	
+	#save_image(c_tensor, 'content.jpg', nrow=1)
+	#save_image(s_tensor, 'style.jpg', nrow=1)
+
+	with torch.no_grad():
+		t_start = time()
+		out, c_mask, s_mask = model.run_ST(c, s)
+		t_end = time()
+		print("ST time: ", t_end-t_start)
+		#c_mask = normalize_mask(c_mask)
+		#s_mask = normalize_mask(s_mask)
+		
+
+	out = tensor_to_array(out)
+	imsave(args.output_name, out, quality=100)
+	return;
+	
+if __name__ == '__main__':
+	main()
---
+++ src/decoder.py
@@ -0,0 +1,80 @@
+import copy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Interpolate(nn.Module):
+	def __init__(self, scale_factor=2):
+		super().__init__()
+		self.scale_factor = scale_factor
+
+	def forward(self, x):
+		x = F.interpolate(x, scale_factor=self.scale_factor)
+		return x
+
+
+vgg_decoder_relu5_1 = nn.Sequential(
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(512, 512, 3),
+	nn.ReLU(),
+	Interpolate(2),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(512, 512, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(512, 512, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(512, 512, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(512, 256, 3),
+	nn.ReLU(),
+	Interpolate(2),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(256, 256, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(256, 256, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(256, 256, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(256, 128, 3),
+	nn.ReLU(),
+	Interpolate(2),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(128, 128, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(128, 64, 3),
+	nn.ReLU(),
+	Interpolate(2),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(64, 64, 3),
+	nn.ReLU(),
+	nn.ReflectionPad2d((1, 1, 1, 1)),
+	nn.Conv2d(64, 3, 3)
+	)
+
+
+class Decoder(nn.Module):
+	def __init__(self, level):
+		super().__init__()
+		if level == 1:
+			self.net = nn.Sequential(*copy.deepcopy(list(vgg_decoder_relu5_1.children())[-2:]))
+		elif level == 2:
+			self.net = nn.Sequential(*copy.deepcopy(list(vgg_decoder_relu5_1.children())[-9:]))
+		elif level == 3:
+			self.net = nn.Sequential(*copy.deepcopy(list(vgg_decoder_relu5_1.children())[-16:]))
+		elif level == 4:
+			self.net = nn.Sequential(*copy.deepcopy(list(vgg_decoder_relu5_1.children())[-29:]))
+		elif level == 5:
+			self.net = nn.Sequential(*copy.deepcopy(list(vgg_decoder_relu5_1.children())))
+		else:
+			raise ValueError('level should be between 1~5')
+
+	def forward(self, x):
+		return self.net(x)
