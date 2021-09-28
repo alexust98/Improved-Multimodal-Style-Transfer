46e64907723c4ddb42903c2d4bd8b6fcca6ca811
alexust98 <alex.ustyuzhanin98@yandex.ru>
2021-09-28 16:50:23 +0300

    Argment Parser description changes.

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ from time import time
 
 from src.model import IMST
 from src.utils import normalize_arr
+import matplotlib.pyplot as plt
 
 def main():
 	parser = argparse.ArgumentParser(description='Improved-Multimodal-Style-Transfer')
 	parser.add_argument('--content', '-c', type=str, default=None,
-						help='Content image path e.g. content.jpg.')
+						help='Content image path e.g. contents/content.jpg.')
 	parser.add_argument('--style', '-s', type=str, default=None,
-						help='Style image path e.g. image.jpg.')
+						help='Style image path e.g. styles/style.jpg.')
 	parser.add_argument('--output_name', '-o', type=str, default="results/result.jpg",
-						help='Output path for generated image, no need to add ext, e.g. out.')
+						help='Output path for generated image, e.g. out.jpg.')
 	parser.add_argument('--imsize', type=int, default=512,
-						help='Size of content and style images to be scaled to. E.g. 512x512. Non-square shapes are not supported yet.')
+						help='Size of content and style images to be scaled to, e.g. 512x512. Non-square shapes are not supported yet.')
 	parser.add_argument('--WCT_alpha', '-alpha', type=float, default=1.0,
 						help='WCT procedure content/style fusion proportion, e.g. 1.0 means full stylization, 0.0 would return content image.')
 	parser.add_argument('--save_masks', type=bool, default=False,
@@ -30,9 +31,9 @@ def main():
 	parser.add_argument('--HDBSCAN_cluster_size', "-cluster_size", type=int, default=1500,
 							help='HDBSCAN cluster size hyperparameter, e.g 1500.')
 	parser.add_argument('--gpu', type=int, default=0,
-						help='GPU device id.')
+						help='GPU device id. -1 means cpu.')
 	parser.add_argument('--model_path', type=str, default='model/model_state.pth',
-						help='pretrained model path.')
+						help='Pretrained model path (encoder + decoder).')
 
 	args = parser.parse_args()
 
@@ -74,6 +75,17 @@ def main():
 		out = normalize_arr(out.squeeze(0).transpose(0, 1).transpose(1, 2))
 		
 	imsave(args.output_name, out, quality=100)
+	"""
+	f, ax = plt.subplots(1, 3, figsize=(18, 6))
+	ax[0].imshow(c)
+	ax[0].axis('off')
+	ax[1].imshow(s)
+	ax[1].axis('off')
+	ax[2].imshow(out)
+	ax[2].axis('off')
+	plt.tight_layout()
+	plt.savefig('git_readme.jpg')
+	"""
 	return;
 	
 if __name__ == '__main__':
